1177b65f7815bd91e7bf95eec0358ed7bf84d204
Ríona Stokes <rlsted123@gmail.com>
2021-11-12 11:21:40 +0100

    Filehound finding all files in test directory

--- maintain.js
+++ maintain.js
@@ -1,11 +1,19 @@
 module.exports = () => {
 
-  const Chalk = require('chalk')
+  const Filehound = require('filehound')
   console.log('Running maintain from @seneca/maintain')
 
   function run(){
-    console.log(Chalk.blue('Run function'))
+    console.log('Run function')
+    Filehound.create()
+      .paths(process.cwd())
+      .discard(/node_modules/)
+      .find()
+      .then(files => {
+        files.forEach(file => console.log('Found file', file));
+      })
   }
 
   run()
 }
+// "undefined" is returned if test file calls a console log of module instead of calling module function directly
